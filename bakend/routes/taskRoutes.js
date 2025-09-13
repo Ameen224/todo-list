821d1886fc52a9129ab3f07736c7da2006a0537b
Ameen224 <ameen8714228@gmail.com>
2025-09-13 19:32:48 +0530

    fourth   commit

--- bakend/routes/taskRoutes.js
+++ bakend/routes/taskRoutes.js
@@ -14,14 +14,14 @@ const authMiddleware = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
-router.use(authMiddleware); 
+router.use(authMiddleware);
 
 router.post("/", createTask);
 router.get("/", getallTasks);
 router.get("/:id", getTaskById);
 router.put("/:id", updateTask);
 router.delete("/:id", deleteTask);
-router.patch("/:id/toggle", toggleTaskCompletion); 
+router.patch("/:id/toggle", toggleTaskCompletion);
 
 
 module.exports = router;
--- bakend/server.js
+++ bakend/server.js
@@ -1,28 +1,36 @@
 // bakend/server.js
 const express = require('express');
-const dotenv= require ('dotenv')
-const connectdb= require('./config/db')
+const dotenv = require('dotenv')
+
+// Importing the database connection function
+const connectdb = require('./config/db')
+
+
+const cors = require('cors')
+
+
+// Importing route handlers
 const authroutes = require('./routes/authRoutes')
 const taskroutes = require('./routes/taskRoutes')
-const cors = require('cors')
 dotenv.config()
 connectdb()
 
 
-console.log("JWT_SECRET from env: in server", process.env.JWT_SECRET);
-
-
-
-
+// Creating an Express application
 const app = express();
 app.use(express.json())
 app.use(cors())
 
+
+// Defining routes
 app.use('/api/auth', authroutes)
 app.use('/api/tasks', taskroutes)
 
 const PORT = process.env.PORT || 4000;
 
+
+
+// Starting the server
 app.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
 });
